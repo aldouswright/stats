5189f5198cdcb3356dc3f9eeb575df8f22d392ed
aldouswright <aldous.wright@gmail.com>
2017-05-31 16:44:58 +0800

    R and Stat by HarvardX Week 1 done

--- Week 1/activity_2.r
+++ Week 1/activity_2.r
@@ -1,1 +1,11 @@
 f <- read.csv("femaleMiceWeights.csv")
+f[12,2]
+f$Bodyweight[11]
+idxs <- which(f$Diet == "hf")
+f$Bodyweight[idxs]
+mean(f$Bodyweight[idxs])
+
+
+set.seed(1)
+rand_num = sample(x=13:24,size=1)
+f$Bodyweight[rand_num]
